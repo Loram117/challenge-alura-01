a33f8d7faf568191a6248e3a58fa8d0a8655d189
Loram117 <dougpsx360@gmail.com>
2024-08-03 17:20:39 -0300

    upando arquivos

---
+++ script.js
@@ -0,0 +1,109 @@
+var botaoCriptografar = document.querySelector(".botao__criptografar");
+var botaoDescriptografar = document.querySelector(".botao__descriptografar");
+var imagemSection2 = document.querySelector(".section__2__imagem");
+var paragrafoSection2 = document.querySelector(".section__2__paragrafo");
+var resultado = document.querySelector(".texto__resultado");
+var caixaTexto = document.querySelector(".area__texto")
+
+botaoCriptografar.onclick = criptografar;
+botaoDescriptografar.onclick = descriptografar;
+
+function criptografar() {
+    ocultarArea();
+    var areaTexto = recuperarTexto();
+    resultado.textContent = criptografarTexto(areaTexto);
+    caixaTexto.value = ""
+}
+
+function descriptografar() {
+    ocultarArea();
+    var areaTexto = recuperarTexto();
+    resultado.textContent = descriptografarTexto(areaTexto);
+    caixaTexto.value = ""
+}
+
+function recuperarTexto() {
+    var areaTexto = document.querySelector(".area__texto");
+    return removerAcentos(areaTexto.value.toLowerCase());
+}
+
+function removerAcentos(texto) {
+    return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+function ocultarArea() {
+    imagemSection2.classList.add("ocultar");
+    paragrafoSection2.classList.add("ocultar");    
+}
+
+function criptografarTexto(mensagem) {
+    var texto = mensagem;
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++) {
+        if(texto[i] == "a"){
+            textoFinal +="ai"
+        }
+        else if(texto[i] == "e") {
+            textoFinal +="enter"
+        }
+        else if(texto[i] == "i") {
+            textoFinal +="imes"
+        }
+        else if(texto[i] == "o") {
+            textoFinal +="ober"
+        }
+        else if(texto[i] == "u") {
+            textoFinal +="ufat"
+        }
+        else{
+            textoFinal += texto[i]
+        }
+    }
+    return textoFinal;
+
+}
+
+function descriptografarTexto(mensagem){
+    var texto = mensagem;
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++) {
+        if(texto[i] == "a"){
+            textoFinal += "a"
+            i++;
+        }
+        else if(texto[i] == "e") {
+            textoFinal += "e"
+            i += 4;
+        }
+        else if(texto[i] == "i") {
+            textoFinal += "i"
+            i += 3;
+        }
+        else if(texto[i] == "o") {
+            textoFinal += "o"
+            i += 3;
+        }
+        
+        else if(texto[i] == "u") {
+            textoFinal += "u"
+            i += 3;
+        }
+        else{
+            textoFinal += texto[i];
+        }
+        
+    }
+
+    return textoFinal;
+
+}
+
+const botaoCopiar = document.querySelector(".botao__copiar"); 
+    botaoCopiar.addEventListener("click", () => {
+    var conteudo = document.querySelector(".texto__resultado").textContent;
+    navigator.clipboard.writeText(conteudo);
+    alert("Texto copiado com Sucesso!");
+    
+});
